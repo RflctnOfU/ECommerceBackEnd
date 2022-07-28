6130e4ad933cd7403b1c5a3b7bb660da81d70e42
Kris Marshall <rflctnofu@hotmail.com>
2022-07-28 18:26:54 -0400

    I think this is correct for index.js in models folder

--- models/index.js
+++ models/index.js
@@ -14,11 +14,13 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
-ProductTag.belongsToMany(Tag, {
+Product.belongsToMany(Tag, {
+  through: ProductTag,
   foreignKey: 'tag_id',
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
+Tag.belongsToMany(Product, {
+  through: ProductTag,
   foreignKey: 'product_id',
 });
 
